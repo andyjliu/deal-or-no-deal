9d378cb65f528795f56ff7921f9f0db5a1f0ae27
Anna Wang <anna.wang2019+hackathon@gmail.com>
2023-09-30 19:17:54 -0700

    Fix check_validity regex issue

--- negotiation_environment.py
+++ negotiation_environment.py
@@ -83,7 +83,7 @@ class NegotiationEnvironment():
 
     def compute_rewards(self, proposal):
         # Extracting counts of each item for Alice and Bob from the proposal
-        items_pattern = r"(\w+): (\w+|\d+) (book|ball|hat)s? (\w+|\d+) (book|ball|hat)s? (\w+|\d+) (book|ball|hat)s?"
+        items_pattern = r"(\w+): (\w+|\d+) (book|ball|hat)s?,?\s*(\w+|\d+) (book|ball|hat)s?,?\s*(\w+|\d+) (book|ball|hat)s?"
         matches = re.findall(items_pattern, proposal)
 
         if len(matches) != 2:  # Assuming only Alice and Bob are there
